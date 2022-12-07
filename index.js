6ce233e5499c9c8a5f8d44749db38c7f5d16fe47
TrevorStreng <99687886+TrevorStreng@users.noreply.github.com>
2022-12-06 17:36:28 -0800

    theme toggle button

---
+++ index.js
@@ -0,0 +1,28 @@
+
+const toggleBtn = document.getElementById("toggle-btn");
+let darkMode = localStorage.getItem("dark-mode");
+
+const enableDarkMode = () => {
+  document.body.classList.add("dark-mode-theme");
+  toggleBtn.classList.remove("dark-mode-toggle");
+  localStorage.setItem("dark-mode", "enabled");
+};
+
+const disableDarkMode = () => {
+  document.body.classList.remove("dark-mode-theme");
+  toggleBtn.classList.add("dark-mode-toggle");
+  localStorage.setItem("dark-mode", "disabled");
+};
+
+if (darkMode === "enabled") {
+  enableDarkMode(); // set state of darkMode on page load
+}
+
+toggleBtn.addEventListener("click", (e) => {
+  darkMode = localStorage.getItem("dark-mode"); // update darkMode when clicked
+  if (darkMode === "disabled") {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
+});
