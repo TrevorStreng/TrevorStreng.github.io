85ee2af22e42d33181c6fdd8386bf3453b85067f
TrevorStreng <99687886+TrevorStreng@users.noreply.github.com>
2022-12-06 17:49:35 -0800

    dark mode update1

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-
+const container = document.querySelector('.container')
 const toggleBtn = document.getElementById("toggle-btn");
 let darkMode = localStorage.getItem("dark-mode");
 
 const enableDarkMode = () => {
   document.body.classList.add("dark-mode-theme");
+  container.classList.add("dark-mode-container");
   toggleBtn.classList.remove("dark-mode-toggle");
   localStorage.setItem("dark-mode", "enabled");
 };
 
 const disableDarkMode = () => {
   document.body.classList.remove("dark-mode-theme");
+  container.classList.remove("dark-mode-container");
   toggleBtn.classList.add("dark-mode-toggle");
   localStorage.setItem("dark-mode", "disabled");
 };
